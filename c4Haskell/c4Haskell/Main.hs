26a2f49bbcaea0a77b3ba7e8dadfcf787f1aff2b
Matthew Montoya <msmontoya2@miners.utep.edu>
2018-12-04 08:48:27 -0700

    Added initial main.hs file

---
+++ c4Haskell/c4Haskell/Main.hs
@@ -0,0 +1,13 @@
+{-------------------------------------------
+-- Created by samuel Tinevra & Matt Montoya
+--------------------------------------------}
+
+module Main where
+    import System.IO
+    import Board
+
+    main = do
+        --set the board to the dimension 6 and 7
+        game (board 6 7) 1
+
+   
